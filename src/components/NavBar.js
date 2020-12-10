40b0300c884c67a409ab5e431039a212bb29b90d
JCJ <jesse.jackson.c@gmail.com>
2020-12-09 23:12:44 -0600

    Creating NavLink within component and establishing routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  return <h1>Navbar Page!</h1>;
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/" exact>
+            Jesse Jackson
+          </NavLink>
+          <NavLink to="/post">Blog Posts</NavLink>
+          <NavLink to="/project">Projects</NavLink>
+          <NavLink to="/about">About Me!</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
